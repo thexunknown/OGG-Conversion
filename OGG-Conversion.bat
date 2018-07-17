972cb3a0465b8e38e0e7174d00cf7e1b7ef7359a
thexunknown <cdavid124213@gmail.com>
2018-07-17 16:28:13 +0100

    Update OGG-Conversion.bat

--- OGG-Conversion.bat
+++ OGG-Conversion.bat
@@ -1,4 +1,4 @@
 SET bitrate=128k
 SET samplerate=48000
 
-for %%A IN (*.mp3 *.wav *.rsd *.flac *.aac *.wma *.wv *.mp2 *. m4a *.mp4 *.mkv *.avi *.wmv) DO ffmpeg -i "%%A" -vn -map_metadata -1 -ar %samplerate% -acodec libvorbis -b:a %bitrate% "%%A.ogg"
+for %%A IN (*.mp3 *.wav *.rsd *.rsp *.flac *.aac *.wma *.wv *.mp2 *. m4a *.mp4 *.mkv *.avi *.wmv) DO ffmpeg -i "%%A" -vn -map_metadata -1 -ar %samplerate% -acodec libvorbis -b:a %bitrate% "%%A.ogg"
